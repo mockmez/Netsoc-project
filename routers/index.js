863b1df64fcf507d79ff9863dc65fa57675429cd
mockmez <mockmez912@gmail.com>
2023-08-29 15:59:50 +0100

    Integrated bootsrap and sass partly with frontend(there still are errors)

--- routers/index.js
+++ routers/index.js
@@ -5,4 +5,9 @@ router.get('/', (req, res) =>{
     res.render('index')
 })
 
+router.get('css-files', (req, res) =>{
+    res.setHeader('Content-Type', 'text/css')
+    res.sendFile(path.join(__dirname, '../..public/assets/css/', 'style.css'))
+})
+
 module.exports = router
