44e567633b7bf82744af12603ef4faeef0bb1792
mockmez <mockmez912@gmail.com>
2023-08-30 10:42:56 +0100

    Added some mobile device accessibility

--- public/index.js
+++ public/index.js
@@ -1,120 +1,108 @@
-const tile_canvas = document.getElementById('tileCanvas')
-const color_palette = document.getElementById('color-palette')
+const tile_canvas = document.getElementById('tileCanvas');
+const color_palette = document.getElementById('color-palette');
+const canvasContainer = document.getElementById('canvas-container');
 
 tile_canvas.width = 1920;
 tile_canvas.height = 1080;
 
-const tile_ctx = tile_canvas.getContext('2d')
+const tile_ctx = tile_canvas.getContext('2d');
 
-const resolution_factor = 2
+const resolution_factor = 2;
 
-const tile_count_x = 16 * resolution_factor
-const tile_count_y = 9 * resolution_factor
+const tile_count_x = 16 * resolution_factor;
+const tile_count_y = 9 * resolution_factor;
 
-const tile_width = tile_canvas.width / tile_count_x
-const tile_height = tile_canvas.height / tile_count_y
+const tile_width = tile_canvas.width / tile_count_x;
+const tile_height = tile_canvas.height / tile_count_y;
 
-//Tile canvas
+// Tile canvas
 
-const tile_arr = []
-let temp_size = 0
-for(let i = 0; i < tile_count_y; i++){
-    for(let j = 0; j < tile_count_x; j++){
-        tile_arr.push(new Tile(j*tile_width, i*tile_height, tile_width, tile_height, 
-            `rgba(${Math.random()*256}, 
-            ${Math.random()*256}, 
-            ${Math.random()*256}, 1)`,
-            temp_size++))
+const tile_arr = [];
+let temp_size = 0;
+for (let i = 0; i < tile_count_y; i++) {
+    for (let j = 0; j < tile_count_x; j++) {
+        tile_arr.push(new Tile(j * tile_width, i * tile_height, tile_width, tile_height,
+            `rgba(${Math.random() * 256},
+            ${Math.random() * 256},
+            ${Math.random() * 256}, 1)`,
+            temp_size++));
     }
 }
 
-console.log('Initial size: ')
-console.log(tile_arr.length)
+// Cursor
 
-//Cursor
+const cursor = new Cursor(tile_width, tile_height, 'white');
+cursor.setCoordinate(tile_arr[0]);
 
-const cursor = new Cursor(tile_width, tile_height, 'white')
-cursor.setCoordinate(tile_arr[0])
+// Events (ASYNCHRONOUS FUNCTIONS)
 
-//Events (ASYNCHRONOUS FUNCTIONS)
-
-//Tile Canvas
+// Tile Canvas
 
 let currentScale = 1;
+let offsetX = 0;
+let offsetY = 0;
+let isDragging = false;
 
+document.addEventListener('wheel', (event) => {
+    if (event.ctrlKey) {
+        event.preventDefault();
 
-document.addEventListener('wheel', (event) =>{
-    
-    if(event.ctrlKey){
-        
-        event.preventDefault()
-
-        // tile_canvas.style.transformOrigin = `${cursor.getTile().getMidX()}px ${cursor.getTile().getMidY()}px`
-        
-        if(event.deltaY < 0){
-            if(currentScale < 10)
-            currentScale *= 1.05
+        if (event.deltaY < 0) {
+            if (currentScale < 10) currentScale *= 1.05;
+        } else if (event.deltaY > 0) {
+            if (currentScale > 0.5) currentScale /= 1.05;
         }
-        else if(event.deltaY > 0){
-            if(currentScale > 0.5)
-            currentScale /= 1.05
-        }
-        
-        updateCanvasTransform(); 
 
-        tile_canvas.style.transform = `scale(${currentScale}) translate(${offsetX}px, ${offsetY}px)`;
+        updateCanvasTransform();
     }
+}, { passive: false });
 
-}, {passive: false})
+tile_canvas.addEventListener('mousedown', (event) => {
+    isDragging = true;
+    startX = event.clientX;
+    startY = event.clientY;
+    offsetX = tile_canvas.offsetLeft;
+    offsetY = tile_canvas.offsetTop;
+    tile_canvas.style.cursor = 'grabbing';
+});
 
-tile_canvas.addEventListener('click', (event) =>{
-    
-    tile_arr.every((tile) =>{
-        if((event.offsetX >= tile.x) && (event.offsetX <= tile.x + tile_width)){
-            if((event.offsetY >= tile.y) &&(event.offsetY <= tile.y + tile_height)){
-                cursor.setCoordinate(tile)
-                return false;
-            }
-        }
+tile_canvas.addEventListener('touchstart', (event) => {
+    if (event.touches.length === 1) {
+        isDragging = true;
+        const touch = event.touches[0];
+        startX = touch.clientX;
+        startY = touch.clientY;
+        offsetX = tile_canvas.offsetLeft;
+        offsetY = tile_canvas.offsetTop;
+        tile_canvas.style.cursor = 'grabbing';
+    }
+});
 
-        return true;
+document.addEventListener('mousemove', (event) => {
+    if (!isDragging) return;
 
-    })
+    const newX = offsetX + event.clientX - startX;
+    const newY = offsetY + event.clientY - startY;
 
-})
+    tile_canvas.style.left = `${newX}px`;
+    tile_canvas.style.top = `${newY}px`;
+});
 
-let isDragging = false
-let startX, startY, offsetX = 0, offsetY = 0
+document.addEventListener('touchmove', (event) => {
+    if (!isDragging || event.touches.length !== 1) return;
 
-tile_canvas.addEventListener('mousedown', (event) =>{
-    isDragging = true
-    startX = event.clientX
-    startY = event.clientY
-    offsetX = tile_canvas.offsetLeft
-    offsetY = tile_canvas.offsetTop
-    tile_canvas.style.cursor = 'grabbing'
-})
+    const touch = event.touches[0];
+    const newX = offsetX + touch.clientX - startX;
+    const newY = offsetY + touch.clientY - startY;
 
-tile_canvas.addEventListener('touchdown', (event) =>{
+    tile_canvas.style.left = `${newX}px`;
+    tile_canvas.style.top = `${newY}px`;
+});
 
-})
-
-document.addEventListener('mousemove', (event) =>{
-    if(!isDragging)
-    return
-
-    const newX = offsetX + event.clientX - startX
-    const newY = offsetY + event.clientY - startY
-
-    tile_canvas.style.left = `${newX}px`
-    tile_canvas.style.top = `${newY}px`
-
-})
-
-document.addEventListener('mouseup', () =>{
-    isDragging = false
-    tile_canvas.style.cursor = 'grab'
-})
+document.addEventListener('mouseup', () => {
+    isDragging = false;
+    tile_canvas.style.cursor = 'grab';
+});
 
 function updateCanvasTransform() {
     const scaleTransform = `scale(${currentScale})`;
@@ -122,15 +110,14 @@ function updateCanvasTransform() {
     tile_canvas.style.transform = `${scaleTransform} ${translateTransform}`;
 }
 
-//ANIMATION LOOP
+// ANIMATION LOOP
 
-function frame_draw(){
-
-    for(let i = 0; i < tile_arr.length; i++){
-        tile_arr[i].draw()
+function frame_draw() {
+    for (let i = 0; i < tile_arr.length; i++) {
+        tile_arr[i].draw();
     }
-    cursor.draw()
-    requestAnimationFrame(frame_draw)
+    cursor.draw();
+    requestAnimationFrame(frame_draw);
 }
 
-frame_draw()
+frame_draw();
