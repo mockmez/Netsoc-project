87a6964c38d4fd300112fe17900d89a97ad4770c
mockmez <mockmez912@gmail.com>
2023-09-17 23:13:08 +0100

    removed admin model from .gitignore

---
+++ model/admin.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const adminLoginSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model("db_admin", adminLoginSchema);
